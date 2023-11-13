a7c552be48f84023329e185dbbf661afd4369c4a
Niki Hietala <niki.hietala@gmail.com>
2023-11-13 15:58:12 +0200

    pylint

--- src/varasto.py
+++ src/varasto.py
@@ -34,11 +34,8 @@ class Varasto:
         if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
-
-            return kaikki_mita_voidaan
-
+            return kaikki_mita_voidaan  
         self.saldo = self.saldo - maara
-
         return maara
 
     def __str__(self):
